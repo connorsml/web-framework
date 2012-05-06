df5bc12d2a717bb21c4095fb7645bb00d72da084
Alexei Sholik <alcosholik@gmail.com>
2012-05-06 18:38:08 +0300

    Move the code around

--- 2-queries/src/feb.ex
+++ 2-queries/src/feb.ex
@@ -5,6 +5,8 @@ defmodule Feb do
   #
   # expands to.
   defmacro __using__(module, opts) do
+    Module.add_compile_callback module, __MODULE__, :default_handle
+
     root_val = Keyword.get(opts, :root, ".")
 
     quote do
@@ -18,66 +20,11 @@ defmodule Feb do
     end
   end
 
-  def start(module) do
+  def start(_) do
     IO.puts "Executing Feb.start"
-    pid = spawn __MODULE__, :init, [module]
-    Process.register module, pid
-    pid
-  end
-
-
-  def init(module) do
-    msg_loop module
-  end
-
-  defp msg_loop(module) do
-    receive do
-    match: { from, { :get, path_with_query } }
-      { path, query } = split_path(path_with_query)
-      from <- module.handle(:get, path, query)
-      msg_loop module
-
-    match: { from, { :post, path, body } }
-      from <- module.handle(:post, path, body)
-      msg_loop module
-
-    match: { from, { :delete, path } }
-      from <- module.handle(:delete, path)
-      msg_loop module
-    end
-  end
-
-  # Return { path, query } where `query` is an orddict.
-  def split_path(path_with_query) do
-    case Regex.split %r/\?/, path_with_query do
-    match: [ path, query ]
-      { path, dict_from_query(query) }
-
-    # No query in the path. Return an empty orddict.
-    match: [ path ]
-      { path, Orddict.new }
-    end
-  end
-
-  # Split the query of the form `key1=value1&key2=value2...` into separate
-  # key-value pairs and put them in an orddict
-  defp dict_from_query(query) do
-    parts = Regex.split %r/&/, query
-    Enum.reduce parts, Orddict.new, fn(kvstr, dict) ->
-      [ key, value ] = Regex.split %r/=/, kvstr
-      Dict.put dict, key, value
-    end
   end
 
   ###
-
-  defmacro get(path, query, [do: code]) do
-    quote do
-      def handle(:get, unquote(path), unquote(query)) do
-        unquote(code)
-      end
-    end
-  end
 
   # GET request handler. It has two forms.
   defmacro get(path, [do: code]) do
@@ -102,28 +49,67 @@ defmodule Feb do
     end
   end
 
+  # A 2-argument handler that also receives a query along with the path
+  defmacro get(path, query, [do: code]) do
+    quote do
+      def handle(:get, unquote(path), unquote(query)) do
+        unquote(code)
+      end
+    end
+  end
+
   # POST request handler
   defmacro post(path, [do: code]) do
-    # Disable hygiene so that `_body` is accessible in the client code
+    # Disable hygiene so that `_data` is accessible in the client code
     quote hygiene: false do
-      def handle(:post, unquote(path), _body) do
+      def handle(:post, unquote(path), _data) do
         unquote(code)
       end
     end
   end
 
   # Generic request handler
-  #  defmacro handle(path, query, body, block) do
-  #    quote do
-  #    end
-  #  end
+  defmacro multi_handle(path, blocks) do
+    # Remove the entry for `:do` which is nil in this case
+    blocks = Keyword.delete blocks, :do
 
-  defmacro default_handle do
+    # Iterate over each block in `blocks` and produce a separate `handle`
+    # clause for it
+    Enum.map blocks, fn ->
+    match: {:get, code}
+      quote hygiene: false do
+        def handle(:get, unquote(path), _query) do
+          unquote(code)
+        end
+      end
+
+    match: {:post, code}
+      quote hygiene: false do
+        def handle(:post, unquote(path), _data) do
+          unquote(code)
+        end
+      end
+    end
+  end
+
+  ###
+
+  # Return { path, query } where `query` is an orddict.
+  def split_path(path_with_query) do
+    uri_info = URI.parse path_with_query
+    { uri_info.path, URI.decode_query(uri_info.query || "") }
+  end
+
+  ###
+
+  # Default catch-all handler for request not handled explicitly by the client
+  # code
+  defmacro default_handle(_) do
     quote do
       def handle(method, path, data // "")
       def handle(method, path, data) do
         # Allow only the listed methods
-        if not (method in [:get, :post, :delete]) do
+        if not (method in [:get, :post]) do
           format_error(400)
 
         # Path should always start with a slash (/)
@@ -151,33 +137,5 @@ defmodule Feb do
     else:
       "503 Internal Server Error"
     end }
-  end
-
-  ###
-
-  defmodule API do
-    # Client API
-
-    def get(target, path_with_query) do
-      call target, { :get, path_with_query }
-    end
-
-    def post(target, path, body // "") do
-      call target, { :post, path, body }
-    end
-
-    def delete(target, path) do
-      call target, { :delete, path }
-    end
-
-    def call(target, msg) do
-      target <- { Process.self(), msg }
-      receive do
-      match: x
-        x
-      after: 1000
-        :timeout
-      end
-    end
   end
 end
--- 2-queries/src/server.ex
+++ 2-queries/src/server.ex
@@ -1,53 +1,34 @@
 defmodule HelloServer do
   use Feb, root: "assets"
 
-  get "/", query do
-    search = Dict.get query, "search"
-    if search do
-      { :ok, "No items found for the query '#{search}'" }
-    else:
-      { :ok, "Hello world!" }
-    end
-  end
-
-  get "/idontcare" do
-    :ok
+  get "/" do
+    { :ok, "Hello world!" }
   end
 
   get "/demo", file: "demo.html"
 
   post "/" do
-    { :ok, "You're posted!\nYour data: #{inspect _body}" }
+    { :ok, "You're posted!\nYour data: #{inspect _data}" }
   end
 
-  ###
+  ## New stuff below this line ##
 
-  #  handle "/kvstore", query, body do
-  #  post:
-  #    Enum.each query, fn({k, v}) ->
-  #      Erlang.ets.insert :simple_table, {k, v}
-  #    end
-  #    :ok
-  #
-  #  get:
-  #    key = Dict.get query, "key"
-  #    if key do
-  #      [val] = Erlang.ets.lookup :simple_table, key
-  #      { :ok, val }
-  #    else:
-  #      { :error, 404 }
-  #    end
-  #
-  #  delete:
-  #    key = Dict.get query, "key"
-  #    if key do
-  #      Erlang.ets.delete :simple_table, key
-  #      :ok
-  #    else:
-  #      { :error, 404 }
-  #    end
-  #  end
+  multi_handle "/kvstore" do
+  post:
+    IO.puts "Got a POST request with data: #{inspect _data}"
+    :ok
 
-  default_handle
+  get:
+    IO.puts "Got a GET request with query: #{inspect _query}"
+    :ok
+  end
 
+  get "/search", query do
+    search = Dict.get query, "q"
+    if search do
+      { :ok, "No items found for the query '#{search}'" }
+    else:
+      { :ok, "No query" }
+    end
+  end
 end
